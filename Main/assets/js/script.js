2292baa902062efb6bbf761fcfbd57b8585cf69e
Qais-cyberlife <qaiscyberlife@gmail.com>
2023-01-26 16:21:39 -0500

    last 4 games slider done and hero section 1st slide done

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -3,9 +3,9 @@
 
 
   function initMap() {
-    var qatar = {lat: 25.3548, lng: 51.1839};
+    var qatar = {lat: 25.3089, lng: 51.5283389031074};
      var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 8,
+      zoom: 10,
       center: qatar
     });
   }
